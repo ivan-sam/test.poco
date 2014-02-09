281c61f4204c7a51400287f39a6ceb9b8658e8ad
Ivan Samygin <ivan.sam@gmail.com>
2014-02-09 22:33:52 +0400

    Test(Poco): Added http functions.

--- src/SampleServer.cpp
+++ src/SampleServer.cpp
@@ -39,6 +39,16 @@
 #include "Poco/Task.h"
 #include "Poco/TaskManager.h"
 #include "Poco/DateTimeFormatter.h"
+#include "Poco/Net/HTTPClientSession.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include <Poco/Net/HTTPCredentials.h>
+#include "Poco/StreamCopier.h"
+#include "Poco/NullStream.h"
+#include "Poco/Path.h"
+#include "Poco/URI.h"
+#include "Poco/Exception.h"
+
 #include <iostream>
 
 
@@ -51,7 +61,67 @@ using Poco::Util::HelpFormatter;
 using Poco::Task;
 using Poco::TaskManager;
 using Poco::DateTimeFormatter;
+using Poco::Net::HTTPClientSession;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPMessage;
+using Poco::StreamCopier;
+using Poco::Path;
+using Poco::URI;
+using Poco::Exception;
 
+bool doRequest(Poco::Net::HTTPClientSession& session, Poco::Net::HTTPRequest& request, Poco::Net::HTTPResponse& response, std::istream& result_stream)
+{
+	session.sendRequest(request);
+	std::istream& rs = session.receiveResponse(response);
+	std::cout << response.getStatus() << " " << response.getReason() << std::endl;
+	if (response.getStatus() != Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED)
+	{
+		result_stream = rs;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+getHttpResponse(const std::string& url_text)
+{
+	try
+	{
+		URI uri(url_text);
+		std::string path(uri.getPathAndQuery());
+		if (path.empty()) path = "/";
+
+        std::string username;
+        std::string password;
+        Poco::Net::HTTPCredentials::extractCredentials(uri, username, password);
+        Poco::Net::HTTPCredentials credentials(username, password);
+
+		HTTPClientSession session(uri.getHost(), uri.getPort());
+		HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+		HTTPResponse response;
+		std::istream& result_stream = NULL;
+		if (!doRequest(session, request, response, result_stream))
+		{
+            credentials.authenticate(request, response);
+			if (!doRequest(session, request, response, result_stream))
+			{
+				std::cerr << "Invalid username or password" << std::endl;
+				return 1;
+			}
+		}
+		
+		StreamCopier::copyStream(rs, std::cout);
+	}
+	catch (Exception& exc)
+	{
+		std::cerr << exc.displayText() << std::endl;
+		return 1;
+	}
+	return 0;
+}
 
 class SampleTask: public Task
 {
